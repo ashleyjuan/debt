b39d7081200836a86653e393587c1b9191b8ec30
juan <ashley910521@gmail.com>
2023-04-24 21:44:13 +0800

    no message

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import Login from './Login';
+import Register from './Register';
+import ForgetPassword from './Forget_Password';
+import MainOutlet from './Layout';
+import DebtInformation from './Debt_Information';
+import Charts from './Charts';
+import Friends from './Friends';
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/www'}>
+          <Route index element={<Login />} />
+          <Route path={'/Login'} element={<Login />} />
+          <Route path={'/Register'} element={<Register />} />
+          <Route path={'/Forget_Password'} element={<ForgetPassword />} />
+          <Route path={''} element={<MainOutlet />}>
+            <Route path={'Debt_Information'} element={<DebtInformation />} />
+            <Route path={'Charts'} element={<Charts />} />
+            <Route path={'Friends'} element={<Friends />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
